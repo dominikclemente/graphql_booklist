1fc693b2aec27d7c2134bccd8db3d1c8f94ba275
Dominik Clemenete <dominik.clemente@gmail.com>
2020-07-20 10:27:33 +0100

    feat(mlab_mongodb): Connect GraphQL to MongoDB database on Mlab servers and rewrite all search queries within GraphQL

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
 
 const app = express();
+
+const MLAB_URI = 'mongodb+srv://dominik:graphQL@gql-demo.ykoag.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(MLAB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
