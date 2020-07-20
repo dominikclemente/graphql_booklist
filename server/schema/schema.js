8c0ea37446a1445d12ec529297cd5cd4bff3f0ca
Dominik Clemenete <dominik.clemente@gmail.com>
2020-07-20 10:28:32 +0100

    fix(cleanup): remove unnecessary comments

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -34,7 +34,6 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                //return _.filter(books, {authorId: parent.id});
                 return Book.find({authorId: parent.id});
             }
         }
@@ -48,7 +47,6 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                //return _.find(books, {id: args.id});
                 return Book.findById(args.id);
             }
         },
@@ -56,21 +54,18 @@ const RootQuery = new GraphQLObjectType({
             type: AuthorType,
             args: {id: {type: GraphQLID}},
             resolve(parents, args) {
-                //return _.find(authors, {id: args.id});
                 return Author.findById(args.id);
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args) {
-                // return books;
                 return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args) {
-                // return authors;
                 return Author.find({});
             }
         }
